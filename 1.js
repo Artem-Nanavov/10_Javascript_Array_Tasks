88c09363ef487f77daf29fcf6f27b965bb5154c3
Artem Nanavov <artemnanavov@gmail.com>
2020-09-24 20:34:22 +0300

    1

---
+++ 1.js
@@ -0,0 +1,14 @@
+/**
+ * Описание задачи: Напишите функцию, которая заполняет новый массив предоставленным значением.
+ * Ожидаемый результат: (3, 'a') => ['a', 'a', 'a']
+ * Сложность задачи: 1 of 5
+ *
+ * @param { number } arraySize - размер массива
+ * @param { any } value - значение для массива
+ * @returns { any[] }
+ */
+const fill = (arraySize, value) => Array(arraySize).fill(value);
+
+const data = 32;
+const valueToFill = 123;
+console.log(fill(data, valueToFill)); // ['a', 'a', 'a']
